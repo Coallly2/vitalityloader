dfc8960bb559d9132b77acdf9cec6555300a11f9
Coallly2 <rraindeer125@gmail.com>
2026-07-30 06:05:24 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,27 +1,25 @@
 local Scripts = {
-    [1119466531] = "https://raw.githubusercontent.com/Coallly2/vitalityloader/Loader/1119466531.lua", -- Legend's of Speed
-    [10256263771] = "https://raw.githubusercontent.com/Coallly2/vitalityloader/Loader/10256263771.lua" -- Push Boulder
+    [1119466531] = "https://raw.githubusercontent.com/Coallly2/vitalityloader/Loader/1119466531.lua",
+    [10256263771] = "https://raw.githubusercontent.com/Coallly2/vitalityloader/Loader/10256263771.lua"
 }
 
-local url = Scripts[game.GameId]
+local id = tonumber(game.GameId)
+
+print("GameId:", id)
+
+local url = Scripts[id]
 
 if not url then
-    warn(("No script available for GameId %d"):format(game.GameId))
+    warn("No script available for GameId "..id)
     return
 end
 
-local success, source = pcall(function()
-    return game:HttpGet(url)
-end)
-
-if not success then
-    warn("Failed to download script:", source)
-    return
-end
+local source = game:HttpGet(url)
 
 local func, err = loadstring(source)
+
 if not func then
-    warn("Failed to compile script:", err)
+    warn("Compile error:", err)
     return
 end
 
